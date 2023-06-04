aecaef1cb3e55913f863448f4b493243e2ae7e89
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2023-06-04 15:12:38 +0700

    update sth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,6 +48,9 @@ export class AppComponent {
                     }
                 });
             }
+            // else {
+            //     //Viết code ở đây
+            // }
 
             //this.getPfSv.getProfileLinkedIn1(this.linkProfileLinkedin);
         }else {
