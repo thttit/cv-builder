4d0e70b0f62f7d02f86a6be6735d28f9c09bf23a
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2023-08-02 13:51:37 +0700

    edit info variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ import html2canvas from 'html2canvas';
 //Từ "selector .... styleUrls": Metadata
 export class AppComponent {
   pageGetProfile: any;
-  profileUserLinkedIn: any;
-  linkProfileLinkedin: any;
+  profileUser: any;
+  linkProfile: any;
   profilePicUrl: any;
   fullName: any;
   studyAt: any;
@@ -23,7 +23,6 @@ export class AppComponent {
   skills: any;
   experiences: any;
   educations: any;
-  companies: any;
   constructor(private getPfSv: GetProfileService) {}
 
   @ViewChild('cvInfo', { static: false }) el!: ElementRef;
@@ -31,26 +30,43 @@ export class AppComponent {
   onChangeInputProfileLink() {}
 
   async buildCVPDF() {
-    if (this.linkProfileLinkedin && this.pageGetProfile) {
+    if (this.linkProfile && this.pageGetProfile) {
       if (this.pageGetProfile == 'linkedin') {
         this.getPfSv
-          .getProfileLinkedIn(await this.linkProfileLinkedin)
+          .getProfileLinkedIn(await this.linkProfile)
           .subscribe((res) => {
             if (res) {
               alert('Build CV successfully');
-              this.profileUserLinkedIn = res;
-              this.profilePicUrl = this.profileUserLinkedIn.profile_pic_url;
-              this.fullName = this.profileUserLinkedIn.full_name;
-              this.studyAt = this.profileUserLinkedIn.occupation;
-              this.city = this.profileUserLinkedIn.city;
-              this.country = this.profileUserLinkedIn.country_full_name;
-              this.experiences = this.profileUserLinkedIn.experiences;
-              this.educations = this.profileUserLinkedIn.education;
-              this.skills = this.profileUserLinkedIn.skills;
+              this.profileUser = res;
+              this.profilePicUrl = this.profileUser.profile_pic_url;
+              this.fullName = this.profileUser.full_name;
+              this.studyAt = this.profileUser.occupation;
+              this.city = this.profileUser.city;
+              this.country = this.profileUser.country_full_name;
+              for(let i = 0; i < this.profileUser.experiences.length; i++) {
+                let experience = {
+                  company: this.profileUser.experiences[i].company,
+                  position: this.profileUser.experiences[i].title
+                }
+                this.experiences.push(experience);
+              }
+              for(let i = 0; i < this.profileUser.education.length; i++) {
+                let education = {
+                  school: this.profileUser.education[i].school,
+                  fieldOfStudy: this.profileUser.education[i].field_of_study
+                }
+                this.educations.push(education);
+              }
+              for(let i = 0; i < this.profileUser.skills.length; i++) {
+                let skill = {
+                  name: this.profileUser.skills[i]
+                }
+                this.skills.push(skill);
+              }
             }
           });
       } else if (this.pageGetProfile == 'orcid') {
-        var splitted = this.linkProfileLinkedin.split('/');
+        var splitted = this.linkProfile.split('/');
         var IdORCID = splitted[splitted.length - 1];
 
         //test: https://orcid.org/0009-0007-1859-8716
@@ -64,20 +80,20 @@ export class AppComponent {
 
             if (res) {
               alert('Build CV successfully');
-              this.profileUserLinkedIn = res;
+              this.profileUser = res;
               this.fullName =
-                this.profileUserLinkedIn.person.name['family-name'].value +
-                this.profileUserLinkedIn.person.name['given-names'].value;
-              //   this.studyAt = this.profileUserLinkedIn.occupation;
-              //   this.city = this.profileUserLinkedIn.city;
+                this.profileUser.person.name['family-name'].value +
+                this.profileUser.person.name['given-names'].value;
+              //   this.studyAt = this.profileUser.occupation;
+              //   this.city = this.profileUser.city;
               this.country =
-                this.profileUserLinkedIn.person.addresses.address[0].country.value;
+                this.profileUser.person.addresses.address[0].country.value;
               this.experiences =
-                this.profileUserLinkedIn['activities-summary'].employments[
+                this.profileUser['activities-summary'].employments[
                   'affiliation-group'
                 ][0].summaries[0]['employment-summary'].organization.name;
               this.educations =
-                this.profileUserLinkedIn['activities-summary'].educations[
+                this.profileUser['activities-summary'].educations[
                   'affiliation-group'
                 ][0].summaries[0]['education-summary'].organization.name;
 
@@ -93,14 +109,14 @@ export class AppComponent {
       //     //Viết code ở đây
       // }
 
-      //this.getPfSv.getProfileLinkedIn1(this.linkProfileLinkedin);
+      //this.getPfSv.getProfileLinkedIn1(this.linkProfile);
     } else {
       alert('Vui lòng nhập link profile');
     }
   }
 
   downloadCVPDF() {
-    if (this.profileUserLinkedIn) {
+    if (this.profileUser) {
       html2canvas(this.el.nativeElement).then((canvas) => {
         const contentDataUrl = canvas.toDataURL('image/png');
         let pdf = new jsPDF('p', 'mm', 'a4');
