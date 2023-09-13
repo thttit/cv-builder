55591ff433bfcb4fe55eae06a0c4b99ebe18a0ff
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2023-09-13 17:26:16 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { GetProfileService } from './services/get-profile.service';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 import { AuthService } from './services/auth.service';
-import { environment } from "../environments/environment";
+import { environment } from '../environments/environment';
 
 @Component({
   //Decurator
@@ -26,7 +26,10 @@ export class AppComponent {
   skills: any[] = [];
   experiences: any[] = [];
   educations: any[] = [];
-  constructor(private getPfSv: GetProfileService, private authToken: AuthService) {}
+  constructor(
+    private getPfSv: GetProfileService,
+    private authToken: AuthService
+  ) {}
 
   @ViewChild('cvInfo', { static: false }) el!: ElementRef;
 
@@ -37,42 +40,42 @@ export class AppComponent {
       if (this.pageGetProfile == 'linkedin') {
         this.isLoading = true;
         this.authToken.accessToken = environment.linkedin_token;
-        this.getPfSv
-        .getProfileLinkedIn(await this.linkProfile)
-        .subscribe((res) => {
-          if (res) {
+        this.getPfSv.getProfileLinkedIn(await this.linkProfile).subscribe(
+          (res) => {
+            if (res) {
+              this.isLoading = false;
+              alert('Build CV successfully');
+              this.profileUser = res;
+              this.profilePicUrl = this.profileUser.profile_pic_url;
+              this.fullName = this.profileUser.full_name;
+              this.studyAt = this.profileUser.occupation;
+              this.city = this.profileUser.city;
+              this.country = this.profileUser.country_full_name;
+              for (let i = 0; i < this.profileUser.experiences.length; i++) {
+                let experience = {
+                  company: this.profileUser.experiences[i].company,
+                  position: this.profileUser.experiences[i].title,
+                };
+                this.experiences.push(experience);
+              }
+              for (let i = 0; i < this.profileUser.education.length; i++) {
+                let education = {
+                  school: this.profileUser.education[i].school,
+                  fieldOfStudy: this.profileUser.education[i].field_of_study,
+                };
+                this.educations.push(education);
+              }
+              for (let i = 0; i < this.profileUser.skills.length; i++) {
+                let skill = {
+                  name: this.profileUser.skills[i],
+                };
+                this.skills.push(skill);
+              }
+            }
+          },
+          (error) => {
             this.isLoading = false;
-            alert('Build CV successfully');
-            this.profileUser = res;
-            this.profilePicUrl = this.profileUser.profile_pic_url;
-            this.fullName = this.profileUser.full_name;
-            this.studyAt = this.profileUser.occupation;
-            this.city = this.profileUser.city;
-            this.country = this.profileUser.country_full_name;
-            for(let i = 0; i < this.profileUser.experiences.length; i++) {
-              let experience = {
-                company: this.profileUser.experiences[i].company,
-                position: this.profileUser.experiences[i].title
-              }
-              this.experiences.push(experience);
-            }
-            for(let i = 0; i < this.profileUser.education.length; i++) {
-              let education = {
-                school: this.profileUser.education[i].school,
-                fieldOfStudy: this.profileUser.education[i].field_of_study
-              }
-              this.educations.push(education);
-            }
-            for(let i = 0; i < this.profileUser.skills.length; i++) {
-              let skill = {
-                name: this.profileUser.skills[i]
-              }
-              this.skills.push(skill);
-            }
-          }
-        }, (error) => {
-          this.isLoading = false;
-            alert("Error when getting data");
+            alert('Error when getting data');
           }
         );
       } else if (this.pageGetProfile == 'orcid') {
@@ -83,32 +86,44 @@ export class AppComponent {
         //test: https://orcid.org/0009-0007-1859-8716
         this.getPfSv.getProfileORCID(await IdORCID).subscribe(
           (res) => {
-            console.log(res);
-            // const employmentSummaries =
-            //   res['activities-summary'].educations['affiliation-group'][0]
-            //     .summaries[0]['education-summary'].organization.name;
-            // console.log(employmentSummaries);
-
             if (res) {
               alert('Build CV successfully');
               this.profileUser = res;
               this.fullName =
                 this.profileUser.person.name['family-name'].value +
                 this.profileUser.person.name['given-names'].value;
-              //   this.studyAt = this.profileUser.occupation;
-              //   this.city = this.profileUser.city;
-              this.country =
-                this.profileUser.person.addresses.address[0].country.value;
-              this.experiences =
-                this.profileUser['activities-summary'].employments[
-                  'affiliation-group'
-                ][0].summaries[0]['employment-summary'].organization.name;
-              this.educations =
-                this.profileUser['activities-summary'].educations[
-                  'affiliation-group'
-                ][0].summaries[0]['education-summary'].organization.name;
+              this.country = this.profileUser.person.addresses.address[0].country.value;
 
-              console.log(this.educations);
+              let employments = this.profileUser['activities-summary'].employments['affiliation-group'];
+              for (let i = 0; i < employments.length; i++) {
+                let experience = {
+                  organization: employments[i].summaries[0]['employment-summary'].organization.name,
+                  start_year: employments[i].summaries[0]['employment-summary']['start-date'].year.value,
+                  start_month: employments[i].summaries[0]['employment-summary']['start-date'].month.value,
+                  start_day: employments[i].summaries[0]['employment-summary']['start-date'].day.value,
+                  // end_year: employments[i].summaries[0]['employment-summary']['end-date'].year.value,
+                  // end_month: employments[i].summaries[0]['employment-summary']['end-date'].month.value,
+                  // end_day: employments[i].summaries[0]['employment-summary']['end-date'].day.value,
+                  role_title: employments[i].summaries[0]['employment-summary']['role-title'],
+                };
+                this.experiences.push(experience);
+              }
+
+              let edu = this.profileUser['activities-summary'].educations['affiliation-group'];
+              for (let i = 0; i < edu.length; i++) {
+                let education = {
+                  organization: employments[i].summaries[0]['education-summary'].organization.name,
+                  start_year: employments[i].summaries[0]['education-summary']['start-date'].year.value,
+                  start_month: employments[i].summaries[0]['education-summary']['start-date'].month.value,
+                  start_day: employments[i].summaries[0]['education-summary']['start-date'].day.value,
+                  // end_year: employments[i].summaries[0]['education-summary']['end-date'].year.value,
+                  // end_month: employments[i].summaries[0]['education-summary']['end-date'].month.value,
+                  // end_day: employments[i].summaries[0]['education-summary']['end-date'].day.value,
+                  role_title: employments[i].summaries[0]['education-summary']['role-title'],
+                };
+                this.educations.push(education);
+              }
+              console.log(this.profileUser);
             }
           },
           (err) => {
